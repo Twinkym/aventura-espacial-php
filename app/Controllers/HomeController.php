ebc5a9daea549b917b155618b9a41d3dd95b125a
david de la puente enriquez <Twinkymen@hotmail.com>
2024-07-20 19:29:39 +0200

    Update Repo

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 class HomeController {
     public function index() {
+        // Create a new instance of the HomeController class and attach it to the current controller instance.
         require_once 'views/home.php';
     }
 }
--- public/index.php
+++ public/index.php
@@ -2,8 +2,9 @@
 // Cargar el autoload de Composer
 require __DIR__ . '/../vendor/autoload.php';
 
-// Iniciar la aplicación (esto depende de tu estructura y controlador principal)
+// Iniciar la aplicación (esto depende la estructura y controlador principal)
 use App\Controllers\HomeController;
 
+// Crear una instancia del controller y llamaar al metodo adecuado.
 $controller = new HomeController();
 $controller->index();
