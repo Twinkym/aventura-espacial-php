ce7ed402f02e39a11003139a59177b0988071e0e
david de la puente enriquez <Twinkymen@hotmail.com>
2024-07-20 18:30:03 +0200

    Actualizar vercel.json para corregir el despliegue

--- public/index.php
+++ public/index.php
@@ -1,7 +1,8 @@
-<?php 
+<?php
+// Cargar el autoload de Composer
+require __DIR__ . '/../vendor/autoload.php';
 
-require_once '../vendor/autoload.php';
-
+// Iniciar la aplicación (esto depende de tu estructura y controlador principal)
 use App\Controllers\HomeController;
 
 $controller = new HomeController();
